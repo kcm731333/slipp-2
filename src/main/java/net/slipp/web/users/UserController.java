36b15b26b72a0f5aa50cac4e5ecd3a50f027c286
kcm731333 <asdf2245@naver.com>
2018-09-25 16:03:37 +0900

    Model을 통한 java object mapping

--- src/main/java/net/slipp/web/users/UserController.java
+++ src/main/java/net/slipp/web/users/UserController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -19,8 +20,9 @@ public class UserController {
 	@Autowired
 	private UserDao userDao;
 	
-	@RequestMapping(value="/form")
-	public String form(){
+	@RequestMapping("/form")
+	public String form(Model model){
+		model.addAttribute("user", new User());
 		return "users/form";
 	}
 	
